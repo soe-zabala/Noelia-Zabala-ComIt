8c0d2d9558c81216f2590c46424f5145e5620d47
Soe Zabala <nzabalasoledad@gmail.com>
2021-05-19 21:07:21 -0300

    ** resolucion tp 2 - ejercicio 7 **

---
+++ TP2-Ejercicio7/src/MiApp.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class MiApp {
+
+	static Scanner ingresoDatos = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int edad = 0;
+		int experiencia = 0;
+		double remuneracion = 0.0;
+		int opcion = 0;
+
+		System.out.println("ingrese su edad");
+		edad = ingresoDatos.nextInt();
+
+		if (edad < 18 | edad > 35)
+			System.out.println("RECHAZADO - Usted se encuentra fuera del rango de edad solicitado");
+
+		if ((edad >= 18) & (edad <= 35)) {
+			System.out.println("Ingrese la cantidad de años de experiencia en el rubro");
+			experiencia = ingresoDatos.nextInt();
+
+			System.out.println("Ingrese el monto de la remuneración pretendida");
+			remuneracion = ingresoDatos.nextDouble();
+
+			if ((edad >= 18) & (edad <= 32))
+
+				opcion = 1;
+			else
+				opcion = 2;
+
+			
+			
+			switch (opcion) {
+			
+			case 1:
+				if (remuneracion < 50000) {
+					System.out.println("CONDICIONAL - Usted será contratado de forma condicional");
+				} else {
+					System.out.println("RECHAZADO - Usted excede los valores de remuneración para el puesto");
+				}
+				break;
+
+			case 2:
+
+				if ((experiencia < 6) & (experiencia > 3)) {
+					System.out.println("CONDICIONAL - Usted será contratado de forma condicional");
+				} else {
+					System.out.println(
+							"RECHAZADO - Usted no cumple con los años de experiencia solicitados para el puesto");
+
+				}
+				break;
+			}
+		}
+	}
+
+}
